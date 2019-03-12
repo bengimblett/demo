4bbcf887264744fdb1e9192496067f153c3b0315
Ben Gimblett <begim@microsoft.com>
2019-03-12 09:51:30 +0000

    initial project

---
+++ git hub aspnetcore demo ci cd with az devops/git hub aspnetcore demo ci cd with az devops/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace git_hub_aspnetcore_demo_ci_cd_with_az_devops.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ git hub aspnetcore demo ci cd with az devops/git hub aspnetcore demo ci cd with az devops/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace git_hub_aspnetcore_demo_ci_cd_with_az_devops.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
